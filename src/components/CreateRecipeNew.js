cc69ad7d03d8bfc0c13443a71472310851e45b80
timmy <timmy.mortensen@hotmail.com>
2023-05-16 12:55:00 +0200

    Update CreateRecipeNew.js

--- src/components/CreateRecipeNew.js
+++ src/components/CreateRecipeNew.js
@@ -81,6 +81,7 @@ const CreateRecipeNew = ({username}) => {
       return (
       
         <Container>
+          {username}
 
         <h2>Create Recipe</h2>
         <Form onSubmit={handleSubmit}>
