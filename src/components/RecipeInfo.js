27dfbb5cc5bf93c529b642dd32a61ab87b134f5e
timmy <timmy.mortensen@hotmail.com>
2023-05-18 13:38:10 +0200

    Update RecipeInfo.js

--- src/components/RecipeInfo.js
+++ src/components/RecipeInfo.js
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import { Button, ListGroup } from 'react-bootstrap';
+import { Button, ListGroup, FormGroup, Form} from 'react-bootstrap';
 import apiFacade from '../apiFacade';
 
 const RecipeInfo = () => {
     const { recipeId } = useParams();
     const [recipe, setRecipe] = useState(null);
+    const [review, setReview] = useState([]);
+    const [reviews, setReviews] = useState([]);
 
 
     useEffect(() => {
@@ -36,7 +38,7 @@ const RecipeInfo = () => {
                         </ListGroup.Item>
 
                         <ListGroup.Item>
-                            <h5>Instriction:</h5>  
+                            <h5>Instruction:</h5>  
                             <span>{recipe.instructions}</span>
                         </ListGroup.Item>
 
@@ -75,14 +77,27 @@ const RecipeInfo = () => {
                             <span className='review-description'>{review.description}</span>
                         </ListGroup.Item>
                     ))}
+                    <span>review1</span><span>review2</span><span>review3</span><span>review4</span><br/><br/><br/>
                 </ListGroup>
             )}
+            <Form>
+                <FormGroup controlId='review'>
+                    <Form.Label>Add review</Form.Label>
+                    <Form.Control
+                        as="textarea"
+                        rows={3}
+                        placeholder="Enter Review"
+                        value={review}
+                        onChange={(e) => setReview(e.target.value)}
+                        required
+                    />
+                </FormGroup>
 
 
             <br></br>
             <Button onClick={() => handleAddReview()}>Add Review</Button>
             
-            
+            </Form>    
         </div>
     )
 }
